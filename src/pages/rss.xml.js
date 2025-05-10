fc2083609bd943979e81cfbf0c8cfbdc33737098
Hoik <hoik.seo@icloud.com>
2025-05-10 23:23:05 +0900

    rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,15 +4,15 @@ import { getCollection } from "astro:content";
 export async function get() {
   const posts = await getCollection('posts');
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: '낱말로 쌓은 성',
+    description: '오롯이 내 생각과 의견',
+    site: 'https://graywoods.me',
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}/`,
     })),
-    customData: `<language>en-us</language>`,
+    customData: `<language>ko-kr</language>`,
   });
 }
